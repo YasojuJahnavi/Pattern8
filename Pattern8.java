7e558504321a29d82f9a7a4a65483293d9249a06
YasojuJahnavi <yasojujahnavi.jh7@gmail.com>
2025-01-30 19:32:22 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,22 @@
+public class Pattern8 {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i==1&&j==3){
+                    System.out.print("*");
+                }else if (i==2 &&(j==2||j==4)){
+                    System.out.print("*");
+                }else if (i==3 &&(j==1||j==5)){
+                    System.out.print("*");
+                }else if (i==4&&(j==2||j==4)){
+                    System.out.print("*");
+                }else if (i==5&&j==3){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+    }
+}
+}
